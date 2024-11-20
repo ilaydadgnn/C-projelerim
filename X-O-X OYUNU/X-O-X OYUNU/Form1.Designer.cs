1f6789bf52d307ba0f4d1bb7f812d25d9c687fb9
İlayda Doğan <147309826+ilaydadgnn@users.noreply.github.com>
2024-11-20 13:01:58 +0300

    Add files via upload

---
+++ X-O-X OYUNU/X-O-X OYUNU/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace X_O_X_OYUNU
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.DimGray;
+            this.button1.Location = new System.Drawing.Point(28, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 72);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.DimGray;
+            this.button2.Location = new System.Drawing.Point(116, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 72);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.DimGray;
+            this.button3.Location = new System.Drawing.Point(204, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 72);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.DimGray;
+            this.button4.Location = new System.Drawing.Point(28, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 72);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.Color.DimGray;
+            this.button5.Location = new System.Drawing.Point(116, 90);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 72);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gainsboro;
+            this.button6.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.ForeColor = System.Drawing.Color.DimGray;
+            this.button6.Location = new System.Drawing.Point(204, 90);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 72);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Gainsboro;
+            this.button7.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.Color.DimGray;
+            this.button7.Location = new System.Drawing.Point(28, 168);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(82, 72);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Gainsboro;
+            this.button8.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.ForeColor = System.Drawing.Color.DimGray;
+            this.button8.Location = new System.Drawing.Point(116, 168);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(82, 72);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Gainsboro;
+            this.button9.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.ForeColor = System.Drawing.Color.DimGray;
+            this.button9.Location = new System.Drawing.Point(204, 168);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(82, 72);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Toplı_Tik);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(328, 280);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X-O-X";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ X-O-X OYUNU/X-O-X OYUNU/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O_X_OYUNU
+{
+    public partial class Form1 : Form
+    {
+        bool X_sıra;
+        bool O_sıra;
+        int islem_sayisi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void X_belirle()
+        {
+            // yatay başlangıç
+            if(button1.Text=="X" && button2.Text=="X"  && button3.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+
+            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+
+            // yatay bitiş 
+
+
+            // dikey başlangıç
+
+            if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+            if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+
+            // dikey bitiş 
+
+            // çapraz başlangıç
+            if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+            if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                temizle();
+                MessageBox.Show("X kazandı");
+
+            }
+            // çapraz bitiş 
+
+        
+
+        }
+        public void O_belirle()
+        {
+            // yatay başlangıç
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+
+            if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+            if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+
+            // yatay bitiş 
+
+
+            // dikey başlangıç
+
+            if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+            if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+            if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+
+            // dikey bitiş 
+
+            // çapraz başlangıç
+            if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+            if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                temizle();
+                MessageBox.Show("O kazandı");
+
+            }
+            // çapraz bitiş 
+
+
+
+
+
+
+
+        }
+        public void temizle()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+
+            islem_sayisi = 0;
+
+
+
+        }
+        public void beraberlik()
+        {
+            if (islem_sayisi == 9)
+            {
+                temizle();
+                MessageBox.Show("Beraberlik...");
+            }
+
+        }
+
+
+        private void Toplı_Tik(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            islem_sayisi = islem_sayisi + 1;
+
+            if (X_sıra)
+            {
+                X_sıra = false;
+                O_sıra = true;
+                b.Text = "X";
+                b.Enabled = false;
+                X_belirle();
+                beraberlik();
+            }
+            else if (O_sıra)
+           
+            {
+                O_sıra = false;
+                X_sıra = true;
+                b.Text = "O";
+
+                b.Enabled = false;
+                O_belirle();
+                beraberlik();
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X_sıra = true;
+
+        }
+    }
+}
